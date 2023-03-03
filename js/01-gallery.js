d41adf5e6a5cf2c0df6d7d808839917fff9ba993
kadulko <114679506+kadulko@users.noreply.github.com>
2023-03-03 10:30:09 +0100

    JS HW #7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+  )
+  .join("");
+
+gallery.innerHTML = markup;
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  const closeModalWithEsc = () =>
+    gallery.addEventListener("keydown", (event) => {
+      if (event.key == "Escape") instance.close();
+    });
+
+  instance.show(closeModalWithEsc);
+});
 
 console.log(galleryItems);
